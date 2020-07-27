20af712f4ba9413bed2a480dfe0fd797db36c704
arch Virtualbox <arch@vbox>
2020-07-27 14:02:20 +0200

    fix Initscript, initrc

--- .bash_aliases
+++ .bash_aliases
@@ -4,5 +4,5 @@ alias egrep='egrep --color=auto'
 alias vi='vim'
 alias createZip="make clean && zip -r $(pwd|grep -o '[^/]*$').zip . --exclude *.tex --exclude *.png --exclude *.aux --exclude *.log --exclude *.toc --exclude *.exe --exclude *.zip --exclude *.fls --exclude *.fdb_latexmk --exclude *.gz --exclude *.o"
 
-alias config='/usr/bin/git --git-dir=/home/thomas/.cfg/ --work-tree=/home/thomas'
+alias config='/usr/bin/git --git-dir=/home/thomas/.config/.cfg/ --work-tree=/home/thomas'
 alias ls='ls --color=auto --group-directories-first -v '
--- InitScript.sh
+++ InitScript.sh
@@ -1,22 +1,47 @@
 HomeDir=/home/thomas
-mkdir -p $HomeDir/config
-configDirBak=$HomeDir/.config_backup
 
-pacman -S git vim xorg-xinit xorg-server xorg-xset
-git clone --bare https://gitlab.com/Thomas_Niedrist/dotfiles.git $HomeDir/config/.cfg
+configDirBak=$HomeDir/.config_backup
+mkdir -p $configDirBak
+
+if [ -f "$HomeDir/.config" ]; then
+	mv .config $configDirBak
+fi
+mkdir -p $HomeDir/.config/
+
+yes | pacman -S git vim xorg-xinit xorg-server xorg-xset ttf-linux-libertine \
+ttf-dejavu picom ttf-inconsolata
+
+cd /opt
+rm -rf yay-git
+git clone https://aur.archlinux.org/yay-git.git
+chown -R thomas:thomas ./yay-git
+cd yay-git
+sudo -u nobody makepkg -si
+cd $HomeDir
+
+rm -rf st
+git clone https://gitlab.com/Thomas_Niedrist/st.git
+cd st
+make clean install
+cd ..
+
+ls -a | grep -Ev ".config_backup|Initscript.sh" |xargs -t -I '{}' mv {} $configDirBak
+
+rm -rf $HomeDir/.config/.cfg
+git clone --bare https://gitlab.com/Thomas_Niedrist/dotfiles.git $HomeDir/.config/.cfg
 
 function config {
-	/usr/bin/git --git-dir=$HomeDir/config/.cfg --work-tree=$HOME $@
+	/usr/bin/git --git-dir=$HomeDir/.config/.cfg --work-tree=$HomeDir $@
 }
 
-mkdir -p $configDirBak
 config checkout 
-if [ $? = 0 ]; then
-		echo "Config Ckecked Out!";
-else
-		echo "Backing up Files.";
-		config checkout 2>&1 |egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} $configDirBak /{} && config checkout
-fi;
+#if [ $? = 0 ]; then
+#		echo "Config Ckecked Out!";
+#else
+#		echo "Backing up Files.";
+#		config checkout 2>&1 |egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} $$HomeDir/.config/.cfg /{} && config checkout
+#fi;
 
 config config status.showUntrackedFiles no
 chown -R thomas .*
+vim -e -s -c "PlugInstall" -c q
